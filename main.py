d3c12abc07f8d8c2ce85520ff72e54ef83f4e014
Valery Efremov <valeriy131100@gmail.com>
2021-12-13 10:43:08 +0300

    Get wall upload server

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 from environs import Env
+
+import vkapi
 from file_workers import download_image
 
 
@@ -7,7 +9,16 @@ if __name__ == '__main__':
     env = Env()
     env.read_env()
     client_id = env.int('CLIENT_ID')
+    group_id = env.int('GROUP_ID')
     access_token = env('ACCESS_TOKEN')
+
+    upload_server = vkapi.get_wall_upload_server(
+        access_token,
+        api_version='5.131',
+        group_id=group_id
+    )
+
+    print(upload_server)
 
     xkcd_url_template = 'https://xkcd.com/{}/info.0.json'
     python_comic_id = 353
---
+++ vkapi.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def get_wall_upload_server(access_token, api_version, group_id):
+    url = 'https://api.vk.com/method/photos.getWallUploadServer'
+    params = {
+        'access_token': access_token,
+        'v': api_version,
+        'group_id': group_id
+    }
+
+    upload_server = requests.get(url, params=params)
+    return upload_server.json()['response']
+
+
+
