ebecba4bd4bc8a54ad7362a0492d5d1f3094232d
Valery Efremov <valeriy131100@gmail.com>
2021-12-13 10:06:16 +0300

    Get user access token

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from file_workers import download_image
 
 if __name__ == '__main__':
     env = Env()
+    env.read_env()
     client_id = env.int('CLIENT_ID')
+    access_token = env('ACCESS_TOKEN')
 
-    url_template = 'https://xkcd.com/{}/info.0.json'
+    xkcd_url_template = 'https://xkcd.com/{}/info.0.json'
     python_comic_id = 353
-    python_comic_url = url_template.format(python_comic_id)
+    python_comic_url = xkcd_url_template.format(python_comic_id)
     python_comic = requests.get(python_comic_url).json()
     download_image(python_comic['img'], 'Python.png')
     print(python_comic['alt'])
