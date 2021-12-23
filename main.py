a10ec05c59047a12734b534462f3977c2405d535
Valery Efremov <valeriy131100@gmail.com>
2021-12-23 08:44:30 +0300

    Change vkapi variables names

--- main.py
+++ main.py
@@ -8,27 +8,27 @@ from environs import Env
 if __name__ == '__main__':
     env = Env()
     env.read_env()
-    group_id = env.int('GROUP_ID')
-    access_token = env('ACCESS_TOKEN')
-    api_version = '5.131'
+    vk_group_id = env.int('VK_GROUP_ID')
+    vk_access_token = env('VK_ACCESS_TOKEN')
+    vk_api_version = '5.131'
 
     comic = xkcd.get_random_comic()
     comic_filename = xkcd.download_comic(comic)
     comic_message = comic['alt']
 
     comic_attachment = vkapi.upload_photo_to_group_wall(
-        access_token,
-        api_version=api_version,
-        group_id=group_id,
+        vk_access_token,
+        api_version=vk_api_version,
+        group_id=vk_group_id,
         filename=comic_filename
     )
 
     os.remove(comic_filename)
 
     vkapi.wall_post(
-        access_token,
-        api_version=api_version,
-        group_id=group_id,
+        vk_access_token,
+        api_version=vk_api_version,
+        group_id=vk_group_id,
         message=comic_message,
         attachments=comic_attachment
     )
